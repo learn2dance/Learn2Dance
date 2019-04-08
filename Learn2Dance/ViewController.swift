f25c64e24b174e112365c8f4c16815d8b210054b
gabegaerlan <ggaerlan@csumb.edu>
2019-04-08 14:54:55 -0700

    Started Skeleton

---
+++ Learn2Dance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn2Dance
+//
+//  Created by Gabe Gaerlan on 4/8/19.
+//  Copyright © 2019 Gabe Gaerlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
